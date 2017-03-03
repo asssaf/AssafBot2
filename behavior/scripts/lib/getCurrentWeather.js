3b8ab82b68ffa969e8147970d498d938b53cb06d
assaf <asssaf@localhost>
2017-03-02 19:24:50 -0800

    add api key

--- behavior/scripts/lib/getCurrentWeather.js
+++ behavior/scripts/lib/getCurrentWeather.js
@@ -3,7 +3,7 @@
 const request = require('request')
 
 module.exports = function getCurrentWeather(locationName, next) {
-  const appId = '<API KEY>'
+  const appId = 'e4d74b9fc31351f5a87dec5938c17674'
 
   const requestUrl = `http://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${appId}&q=${locationName}`
 
